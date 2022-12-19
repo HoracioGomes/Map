e798b4b196dda5ca05337a0caf7310833ba1522a
Horacio <horacio.junior@ibm.com>
2022-12-19 10:02:17 -0300

    Initial map implementation

--- Mapa/ViewController.swift
+++ Mapa/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let latitude: CLLocationDegrees = -22.895384
+        let longitude: CLLocationDegrees = -42.225164
+        let latitudeDelta: CLLocationDegrees = 0.009
+        let longitudeDelta: CLLocationDegrees = 0.009
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let zoom: MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+        let regiao : MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: zoom)
+        
+        map.setRegion(regiao, animated: true)
+        
+        let anotacao: MKPointAnnotation = MKPointAnnotation()
+        anotacao.coordinate = location
+        anotacao.title = "Abrigo"
+        anotacao.subtitle = "Perto do lago"
+        
+        map.addAnnotation(anotacao)
+        
     }
 
 
